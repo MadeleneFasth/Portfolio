04430bb91982fa6f371ad4ea2c58cf14911aa2c1
MadeleneFasth <113009672+MadeleneFasth@users.noreply.github.com>
2024-09-26 14:05:00 +0200

    new portfolio

---
+++ src/components/Home.jsx
@@ -0,0 +1,58 @@
+import "../style/Home.css";
+import "../style/root.css";
+
+// import ScrolpngText from "./ScrolpngText";
+
+function Home() {
+  return (
+    <section className="home-section">
+      {/* <div className="name-container">
+        <h1 className="pink playfair h2 home-name">Madelene Fasth</h1>
+      </div> */}
+      {/* <ScrolpngText /> */}
+      <div className="home-text-container">
+        {/* <h1 className="h1 pink inter caps home-text ">Frontend Developer/</h1>
+
+        <h1 className="h1 pink inter caps home-text ">UX designer</h1> */}
+        <div className="frontend-text">
+          <p className="light h1  caps inter hovered">f</p>
+          <p className="light h1  caps inter hovered">r</p>
+          <p className="light h1  caps inter hovered">o</p>
+          <p className="light h1  caps inter hovered">n</p>
+          <p className="light h1  caps inter hovered">t</p>
+          <p className="light h1  caps inter hovered">e</p>
+          <p className="light h1  caps inter hovered">n</p>
+          <p className="light h1  caps inter hovered">d</p>
+        </div>
+
+        <div className="developer-text">
+          <p className="light h1  caps inter hovered">d</p>
+          <p className="light h1  caps inter hovered">e</p>
+          <p className="light h1  caps inter hovered">v</p>
+          <p className="light h1  caps inter hovered">e</p>
+          <p className="light h1  caps inter hovered">l</p>
+          <p className="light h1  caps inter hovered">o</p>
+          <p className="light h1  caps inter hovered">p</p>
+          <p className="light h1  caps inter hovered">e</p>
+          <p className="light h1  caps inter hovered">r</p>
+        </div>
+
+        <div className="design-text">
+          <p className="light h1  caps inter hovered">&</p>
+          <p className="light h1  caps inter hovered">U</p>
+          <p className="light h1  caps inter hovered">X</p>
+          <p className="bg-color inter hovered-easter">☺</p>
+          <p className="light h1  caps inter hovered">d</p>
+          <p className="light h1  caps inter hovered">e</p>
+          <p className="light h1  caps inter hovered">s</p>
+          <p className="light h1  caps inter hovered">i</p>
+          <p className="light h1  caps inter hovered">g</p>
+          <p className="light h1  caps inter hovered">n</p>
+          <p className="light h1  caps inter hovered">e</p>
+          <p className="light h1  caps inter hovered">r</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Home;
