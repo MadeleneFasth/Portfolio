c226bdfdadbd28ef5dcda576dd241391bc68f992
MadeleneFasth <113009672+MadeleneFasth@users.noreply.github.com>
2024-09-29 14:00:40 +0200

    ijfi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,7 +27,10 @@ function About() {
         </div>
 
         <div className="image-container">
-          <img src="../blob/docs/assets/profile3.jpg" alt="profile-picture" />
+          <img
+            src="../blob/main/docs/assets/profile3.jpg"
+            alt="profile-picture"
+          />
         </div>
 
         <div className="about-text-container">
